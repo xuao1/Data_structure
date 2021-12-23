fda04dcb3dcc53d9761f339a17509620ef96d441
tom <tom@gmail.com>
2021-12-23 17:21:32 +0800

    need to hand in

---
+++ PB20061343_徐奥_v1/lab01/lab01源代码/离散事件模拟.cpp
@@ -0,0 +1,384 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<algorithm>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<time.h>
+
+typedef struct LNodeQ {//两个窗口的队列的元素，存储客户
+	int ArriveTime;
+	int DurTime;
+	int mount;//办理业务的金额，负为取，正为存
+	struct LNodeQ *next;
+}LNodeQ;
+
+typedef struct {
+	LNodeQ* head, * tail;
+}LinklistQ;//窗口的队列
+
+typedef struct LNodeE {//事件的元素
+	int OccurTime;
+	int NType;//事件类型，0为新客户到达，1为1号窗口客户办理业务，2为号窗口客户办理业务,3为1号窗口离开，4为2号窗口离开
+	struct LNodeE* next;
+}LNodeE;
+
+int total;
+int closetime;
+LNodeE* EventList = new LNodeE;
+LinklistQ Window1;
+LinklistQ Window2;
+int TotalTime, CustomerNum;
+int flag = 1;//第二个窗口无法再办理下一个业务的标记，即有办不完的人一直占用窗口
+int flag2 = 1;//第二个窗口无法再办理下一个业务的标记，即有办不完的人一直占用窗口
+int cnt = 1;//事件数
+int wait;//在考虑第二窗口办理时，需要额外等待的时间
+int Lasttime;//在第二窗口办理业务时，即使第一窗口又来了人，且第一窗口只有一人，这一人也需要等到第二窗口办完才可以办理
+
+void EventInsert(LNodeE en);
+void OpenForDay();//初始化
+bool EventEmpty();//时间表为空，返回1；时间表不空，返回0
+int GetRandomT();//办理业务时长
+void CustomerArrive(LNodeE* q);
+void DeleteWindow(int windownum, LNodeQ& customer);
+void GetWindow(int windownum, LNodeQ& customer);
+void CustomerHandle(LNodeE* q);//知道第几个窗口，以及发生时间，因为是开始办理业务，这里不计算总时间，银行总钱数也不会改变
+void CustomerLeave(LNodeE* q);//当前窗口的顾客办理完业务，要离开
+
+//队列均为带头结点
+int main()
+{//办理业务与离开银行需分开处理，不然会出现bug
+	OpenForDay();
+	LNodeE* q = EventList->next;
+	while (!EventEmpty()) {
+		if (q->NType == 0) {
+			CustomerArrive(q);
+		}
+		else if (q->NType == 3||q->NType==4) {
+			CustomerLeave(q);
+		}
+		else {
+			CustomerHandle(q);
+		} 
+		EventList->next = q->next;
+		q = q->next;
+	}
+	LNodeQ* tmp = Window2.head->next;//遍历第二个窗口的客户
+	LNodeQ customer;
+	while (tmp) {
+		DeleteWindow(2, customer);
+		TotalTime += closetime - customer.ArriveTime;
+		int event_hour = closetime / 60 + 6;
+		int event_min = closetime % 60;
+		printf("\n事件%d:	", cnt++);
+		printf("%d:%02d	一位顾客未能办理业务，离开银行，逗留时间：%d\n", event_hour, event_min, closetime - customer.ArriveTime);
+		printf("		当前银行存有资金总额：%d元\n", total);
+		tmp = tmp->next;
+	}
+	printf("\n事件%d:	", cnt++);
+	int event_hour = closetime / 60 + 6;
+	int event_min = closetime % 60;
+	printf("%d:%02d	营业结束.共计%d位顾客来办理业务，在银行逗留的总时长为%d分钟，平均逗留时间为%.2f分钟\n", event_hour,event_min, CustomerNum, TotalTime, float(TotalTime) / float(CustomerNum));
+	printf("		当前银行存有资金总额：%d元\n", total);
+	return 0;
+}
+
+void EventInsert(LNodeE en)
+{//将某事件按时间由小到大的顺序插入到时间表中
+//	printf("————————%d   %d\n", en.NType, en.OccurTime);
+	//上一条语句在debug中发挥了巨大作用
+	LNodeE* q = EventList->next, * p = EventList;
+	LNodeE* ev = new LNodeE;
+	ev->NType = en.NType; ev->OccurTime = en.OccurTime;
+	while (q) {
+		if (q->OccurTime > en.OccurTime) {
+			p->next = ev; ev->next = q;
+			return;
+		}
+		if (q->OccurTime == en.OccurTime && (en.NType == 4 || en.NType == 3)) {
+			p->next = ev; ev->next = q;
+			return;
+		}//处理二号窗口走，下一位存钱客户同时进来的bug
+		q = q->next;
+		p = p->next;
+	}
+	p->next = ev;
+	ev->next = NULL;
+}
+
+void OpenForDay()
+{//初始化
+	printf("请输入银行初始资金：\n");
+	scanf("%d", &total);
+	printf("请输入营业时长（单位：分钟）：\n");
+	scanf("%d", &closetime);
+	Window1.head = Window1.tail = new LNodeQ;
+	Window2.head = Window2.tail = new LNodeQ;
+	EventList->next = NULL; Window1.head->next = NULL; Window2.head->next = NULL;
+	TotalTime = CustomerNum = 0;
+	LNodeE en;
+	en.NType = 0; en.OccurTime = 0;
+	EventInsert(en);
+	srand((unsigned int)time(NULL));
+	printf("\n事件0:	");
+	printf("6:00	银行开门，开始营业，营业时长为%d分钟\n", closetime);
+	printf("		当前银行存有资金总额：%d元\n", total);
+}
+
+void PrintEventList()
+{
+	LNodeE* q = EventList->next;
+	while (q) {
+		printf("发生时间为：%d  事件类型为：%d\n", q->OccurTime, q->NType);
+		q = q->next;
+	}
+}
+
+bool EventEmpty()
+{//时间表为空，返回1；时间表不空，返回0
+	if (EventList->next) return 0;
+	return 1;
+}
+
+int GetRandomT()//办理业务时长
+{
+	int a = 0;
+	int N;
+	N = (rand() % 111 + rand() % 71) % 15;
+	while (a == 0) {
+		if (N <= 1)	a = rand() % 15 + 1;
+		else if (N <= 4)	a = rand() % 11 + 15;
+		else if (N <= 10)	a = rand() % 11 + 25;
+		else if (N <= 13)	a = rand() % 11 + 35;
+		else a = rand() % 16 + 45;
+	}
+	return a;
+}
+
+int GetRandomM()
+{
+	int a = 0;
+	int b = rand() % 2;
+	int N = (rand() % 111 + rand() % 71) % 10;
+	while (a == 0) {
+		if (N <= 1)	a = rand() % 200 + 1;
+		else if (N <= 5)	a = rand() % 1800 + 200;
+		else if (N <= 7)	a = rand() % 3000 + 2000;
+		else if (N <= 8)	a = rand() % 3000 + 5000;
+		else a = rand() % 20001;
+	}
+	if (b == 1) return a;
+	else return -a;
+}
+
+void CustomerArrive(LNodeE* q)
+{
+	printf("\n事件%d:	", cnt++);
+	++CustomerNum;
+	LNodeQ* nexcus = new LNodeQ;
+	nexcus->ArriveTime = q->OccurTime;
+	int event_hour = nexcus->ArriveTime / 60 + 6;
+	int event_min = nexcus->ArriveTime % 60;
+	nexcus->DurTime = GetRandomT();
+	nexcus->mount = GetRandomM();
+	if (nexcus->mount > 0)	printf("%d:%02d	一位顾客到达银行，办理的业务是存钱%d元,需要办理时长为：%d分钟。\n", event_hour, event_min, nexcus->mount, nexcus->DurTime);
+	if (nexcus->mount < 0)	printf("%d:%02d	一位顾客到达银行，办理的业务是取钱%d元,需要办理时长为：%d分钟。\n", event_hour, event_min, -nexcus->mount, nexcus->DurTime);
+	printf("		当前银行存有资金总额：%d元\n", total);
+	Window1.tail->next = nexcus;
+	Window1.tail = nexcus;
+	Window1.tail->next = NULL;
+	LNodeE en;
+	en.OccurTime = q->OccurTime > Lasttime ? q->OccurTime : Lasttime;
+	//	printf("加入新结点出了问题，以下为输出内容\n");
+	//	printf("Window1.head->next == Window1.tail  %d\n", Window1.head->next == Window1.tail);
+	//	printf("en.OccurTime < closetime  %d\n", en.OccurTime < closetime);
+	//	printf("加入新结点出了问题，以上为输出内容\n");
+	if (Window1.head->next == Window1.tail && en.OccurTime < closetime) {//第一个队伍只有一个顾客，需将这个顾客的办业务事件加入事件表
+
+		en.NType = 1;
+		EventInsert(en);
+	}
+	en.NType = 0; en.OccurTime = nexcus->ArriveTime + GetRandomT();
+	if (en.OccurTime < closetime) EventInsert(en);
+}
+
+void DeleteWindow(int windownum, LNodeQ& customer)
+{
+	if (windownum == 1) {
+		customer.ArriveTime = Window1.head->next->ArriveTime;
+		customer.DurTime = Window1.head->next->DurTime;
+		customer.mount = Window1.head->next->mount;
+		if (Window1.head->next == Window1.tail) { Window1.head->next = NULL; Window1.tail = Window1.head; }
+		else { Window1.head->next = Window1.head->next->next; }
+	}
+	if (windownum == 2) {
+		customer.ArriveTime = Window2.head->next->ArriveTime;
+		customer.DurTime = Window2.head->next->DurTime;
+		customer.mount = Window2.head->next->mount;
+		if (Window2.head->next == Window2.tail) { Window2.head->next = NULL; Window2.tail = Window2.head; }
+		else { Window2.head->next = Window2.head->next->next; }
+	}
+}
+
+void GetWindow(int windownum, LNodeQ& customer)
+{
+	if (windownum == 1) {
+		customer.ArriveTime = Window1.head->next->ArriveTime;
+		customer.DurTime = Window1.head->next->DurTime;
+		customer.mount = Window1.head->next->mount;
+	}
+	if (windownum == 2) {
+		customer.ArriveTime = Window2.head->next->ArriveTime;
+		customer.DurTime = Window2.head->next->DurTime;
+		customer.mount = Window2.head->next->mount;
+	}
+}
+
+void CustomerHandle(LNodeE* q)//知道第几个窗口，以及发生时间，因为是开始办理业务，这里不计算总时间，银行总钱数也不会改变
+{
+	LNodeQ customer;
+	GetWindow(q->NType, customer);//在窗口队列中获取第一个元素,保存在customer中，即customer要办理业务
+	if (q->NType == 1)
+		if (flag == 0 || q->OccurTime + customer.DurTime > closetime) {//第一个窗口办理不完，同时也会一直占用这个窗口
+			flag = 0;
+			LNodeE en;
+			en.NType = 3; en.OccurTime = closetime + 1;
+			EventInsert(en);
+			return;
+		}
+	if (q->NType == 2)
+		if (flag2 == 0 || q->OccurTime + customer.DurTime > closetime) {//第一个窗口办理不完，同时也会一直占用这个窗口
+			flag2 = 0;
+			LNodeE en;
+			en.NType = 4; en.OccurTime = closetime + 1;
+			EventInsert(en);
+			return;
+		}
+	if (q->NType == 1) {//在第一个窗口办业务
+		if (customer.mount >= 0) {
+			wait = customer.DurTime;//此标记为，假如第二个窗口可以办业务，那么第一个窗口的下一个顾客需要再等待的时间
+			int event_hour = q->OccurTime / 60 + 6;
+			int event_min = q->OccurTime % 60;
+			printf("\n事件%d:	", cnt++);
+			printf("%d:%02d	一位顾客开始办理存钱%d元业务\n", event_hour, event_min, customer.mount);
+			printf("		当前银行存有资金总额：%d元\n", total);
+			int total_i = total;
+			total += customer.mount;//第一个窗口发生了存事件，则立即处理第二个窗口的取事件
+			//但是这里的存的金额需要恢复，需要等到办理完业务，这笔钱才算完全存入。
+			LNodeE en;
+			en.NType = 3; en.OccurTime = q->OccurTime + customer.DurTime;
+			EventInsert(en);
+			LNodeQ* tmp = Window2.head->next;//遍历第二个窗口的客户
+			int Window2_num = 0;
+			while (tmp) {
+				Window2_num++;
+				tmp = tmp->next;
+			}
+			tmp = Window2.head->next;
+			LNodeQ* pre = Window2.head;//跟随指针
+			while (Window2_num) {
+				if (tmp->mount + total >= 0) {//假如当前客户的取钱请求可以满足,则将第二个窗口的办理时间加入事件表
+					LNodeE en;
+					en.NType = 2; en.OccurTime = q->OccurTime + wait;//这里也是wait
+					if (en.OccurTime < closetime)	EventInsert(en);
+					wait += tmp->DurTime;
+					total = total + tmp->mount;
+					if (total <= total_i)	break;
+					else {
+						tmp = tmp->next;
+						pre = pre->next;
+						Window2_num--;
+					}
+				}
+				else {//当前顾客的取钱请求无法满足，则将其移动到队伍末尾
+					pre->next = tmp->next;
+					Window2.tail->next = tmp;
+					Window2.tail = tmp;
+					Window2.tail->next = NULL;//这里很重要
+					tmp = pre->next;//跟随指针的作用在这里体现
+					Window2_num--;
+				}
+			}
+			total = total_i;//第二窗口新加的可以办业务的顾客还没有开始办业务，故还没有取钱
+		}
+		if (customer.mount < 0) {
+			if (total + customer.mount >= 0) {
+				wait = customer.DurTime;
+				LNodeE en;
+				en.NType = 3; en.OccurTime = q->OccurTime + customer.DurTime;
+				EventInsert(en);
+				int event_hour = q->OccurTime / 60 + 6;
+				int event_min = q->OccurTime % 60;
+				printf("\n事件%d:	", cnt++);
+				printf("%d:%02d	一位顾客开始在第一窗口办理取钱%d元业务\n", event_hour, event_min, -customer.mount);
+				printf("		当前银行存有资金总额：%d元\n", total);
+			}
+			else {//取钱，不够，将这个顾客加入第二个队列中
+				DeleteWindow(q->NType, customer);//这里很重要，调了很久的bug，这已经从第一队删除了
+				LNodeQ* tmp = new LNodeQ;
+				tmp->ArriveTime = customer.ArriveTime;
+				tmp->DurTime = customer.DurTime;
+				tmp->mount = customer.mount;
+				Window2.tail->next = tmp;
+				Window2.tail = tmp;
+				Window2.tail->next = NULL;
+				//以下步骤一开始被忽略了。当前这个人无法取钱，但是在他之后仍有人排队，需要将他之后的人加入事件中
+				//且下一个人开始办理业务的时间与这个人相同
+				if (Window1.head->next) {
+					LNodeE en;
+					en.NType = 1;
+					en.OccurTime = q->OccurTime > Lasttime ? q->OccurTime : Lasttime;
+					EventInsert(en);
+				}
+			}
+		}
+		Lasttime = q->OccurTime + wait;
+		wait = 0;//这里很重要
+		//至此，处理完了第一个窗口顾客的全部请求，要么顾客离开，要么进入了第二个队列
+	}
+	else if (q->NType == 2) {//第二窗口的顾客办理业务，注意，他们还没取钱，在队列中已经删除
+		LNodeE en;
+		en.NType = 4;
+		en.OccurTime = q->OccurTime + customer.DurTime;
+		int event_hour = q->OccurTime / 60 + 6;
+		int event_min = q->OccurTime % 60;
+		printf("\n事件%d:	", cnt++);
+		printf("%d:%02d	一位顾客开始在第二窗口办理取钱%d元业务\n", event_hour, event_min, -customer.mount);
+		printf("		当前银行存有资金总额：%d元\n", total);
+		EventInsert(en);
+	}
+}
+
+void CustomerLeave(LNodeE* q)//当前窗口的顾客办理完业务，要离开
+{
+	LNodeQ customer;
+	DeleteWindow(q->NType - 2, customer);//在窗口队列中删除第一个元素,保存在customer中，即customer要办理业务
+	if (q->NType == 3) {
+		if (q->OccurTime <= closetime)	total += customer.mount;
+		TotalTime += q->OccurTime - customer.ArriveTime;
+		if (Window1.head->next) {
+			//			printf("一号窗口办理完了业务，且仍有人排队\n");
+			LNodeE en;
+			en.NType = 1;
+			en.OccurTime = q->OccurTime > Lasttime ? q->OccurTime : Lasttime;
+			EventInsert(en);
+		}
+	}
+	if (q->NType == 4) {
+		if (q->OccurTime <= closetime) total += customer.mount;
+		TotalTime += q->OccurTime - customer.ArriveTime;
+	}
+	int event_hour = q->OccurTime / 60 + 6;
+	int event_min = q->OccurTime % 60;
+	printf("\n事件%d:	", cnt++);
+	if (q->OccurTime <= closetime)
+		printf("%d:%02d	一位顾客办理完业务，离开银行，逗留时间：%d\n", event_hour, event_min, q->OccurTime - customer.ArriveTime);
+	else {
+		event_hour = closetime / 60 + 6;
+		event_min = closetime % 60;
+		printf("%d:%02d	一位顾客未能办理业务，离开银行，逗留时间：%d\n", event_hour, event_min, q->OccurTime - customer.ArriveTime);
+	}
+	printf("		当前银行存有资金总额：%d元\n", total);
+
+}
---
+++ PB20061343_徐奥_v1/lab02/lab02源代码/Huffman_final.cpp
@@ -0,0 +1,384 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<fstream>
+using namespace std;
+
+typedef struct {
+	int weight;
+	int parent, lchild, rchild;
+}HTNode;
+typedef HTNode* HuffTree;
+
+char Order;
+unsigned char Char[300];//从1开始存储
+int w_char[300];
+int num_char, num_0, num_8;//字符数，补零数，完整的比特块
+char HC[300][300];
+
+bool Cal_weight(char** argv);//计数每个字符的出现频数
+void Selete(HuffTree HT, int i, int& s1, int& s2);//选取当前权值最小的两个结点
+void InitHuffman(HuffTree& HT);//建立Huffman树
+
+void HuffmanCoding(HuffTree HT, int root, char stack[], int flag, int top);//寻找每个字符的Huffman编码
+void Cal_unsigned(char zero_one[], unsigned char& a);//计算每个比特块的对应的二进制8bits数值
+void EnCoding(char** argv);//Huffman编码
+void Store_all(HuffTree HT);//存储完整的信息，附加信息在文件头
+
+void Read_Text(HuffTree& HT);//将得到的文件进行拆分，为后续解码做准备，建立Huffman树
+void Load_Info(HuffTree& HT);//更新Char[]文件
+bool HuffmanDeCoding(FILE* fp1, FILE* fp2, HuffTree HT, int root);//面向01序列，进行Huffman解码
+void DeCoding(HuffTree HT, char** argv);//先将二进制文件转化为01序列，再将01序列进行Huffman解码
+
+void show_menu();
+
+void Tree_Print(HuffTree HT, int root, int cnt);
+
+int main(int argc, char** argv) 
+{
+	HuffTree HT = NULL;
+	while (1)
+	{
+		show_menu();
+		scanf("%c", &Order);
+		char tmps; scanf("%c", &tmps);
+		if (Order == 'E' || Order == 'e') {
+			if (!Cal_weight(argv)) {//预处理原文件，统计字符出现次数
+				return 0;
+			}
+			InitHuffman(HT);//建立Huffman树
+			char stack[300];
+			HuffmanCoding(HT, 2 * num_char - 1, stack, 2, 0);//求叶子节点的Huffman编码
+			EnCoding(argv);
+			Store_all(HT);
+			cout << "编码已完成！" << endl;
+			continue;
+		}
+		if (Order == 'D' || Order == 'd') {
+			Read_Text(HT);
+			Load_Info(HT);
+			DeCoding(HT, argv);
+			cout << "解码已完成！" << endl;
+			continue;
+		}
+		if (Order == 'P' || Order == 'p') {
+			Tree_Print(HT,2*num_char-1,0);
+			cout << "Huffman树打印成功！" << endl;
+			continue;
+		}
+		if (Order == 'Q' || Order == 'q') {
+			cout << "程序退出" << endl;
+			return 0;
+		}
+		else {
+			cout << "无效的指令，请重新输入。" << endl;
+		}
+	}
+	return 0;
+	//在压缩阶段，需要完成个任务：
+	//1.通过字符型读取原文件，设置数组char表示所有的字符，num_char记录已存储的字符个数，w_char数组表示出现多少次
+	//2.建立Huffman树
+	//3.对原文件进行压缩，二进制写入文件
+	//4.将附加信息存入文件中 
+	//
+	//在解压阶段，需要完成
+	//1.读附件信息文件，建立Huffman树
+	//2.通过二进制读取压缩文件，处理，写入到新文件
+}
+
+bool Cal_weight(char** argv)
+{
+	ifstream inFile(argv[1], ios::in | ios::binary); //二进制读方式打开
+	if (!inFile) {
+		cout << "error" << endl;
+		return 0;
+	}
+	unsigned char p;
+	while (inFile.read((char*)&p, sizeof(p))) { //一直读到文件结束
+		int find_flag = 0;
+		for (int i = 1; i <= num_char; i++) {
+			if (Char[i] == p) {
+				find_flag = 1;
+				w_char[i]++;
+				break;
+			}
+		}
+		if (!find_flag) {
+			num_char++;
+			Char[num_char] = p;
+			w_char[num_char]++;
+		}
+	}
+	inFile.close();
+	return 1;
+}
+
+void Selete(HuffTree HT, int i, int& s1, int& s2)
+{//在1...i-1中选取父节点为0，并且权值最小的两个结点
+	s1 = s2 = 0;
+	HT[0].weight = 0xfffffff;
+	for (int j = 1; j < i; j++) {
+		if (HT[j].parent) continue;
+		if (HT[j].weight < HT[s1].weight) {
+			s2 = s1;
+			s1 = j;
+		}
+		else if (HT[j].weight < HT[s2].weight) {
+			s2 = j;
+		}
+	}
+}
+
+void InitHuffman(HuffTree& HT)
+{
+	int m = num_char * 2 - 1;//树的节点个数
+	HT = new HTNode[m + 1];
+	for (int i = 1; i <= m; i++) {
+		HT[i].weight = (i <= num_char ? w_char[i] : 0);
+		HT[i].lchild = HT[i].rchild = HT[i].parent = 0;
+	}
+	for (int i = num_char + 1; i <= m; i++) {
+		int s1, s2;
+		Selete(HT, i, s1, s2);//在1...i-1中选取父节点为0，并且权值最小的两个结点
+		HT[i].lchild = s1;
+		HT[i].rchild = s2;
+		HT[i].weight = HT[s1].weight + HT[s2].weight;
+		HT[s1].parent = HT[s2].parent = i;
+	}
+}
+
+void HuffmanCoding(HuffTree HT, int root, char stack[], int flag, int top)
+{//用栈来记录01序列，flag==0,向左儿子走，flag==1,向右儿子走
+	if (flag == 0) {
+		stack[top] = '0';
+		top++;
+	}
+	if (flag == 1) {
+		stack[top] = '1';
+		top++;
+	}
+	if (HT[root].lchild == 0) {//当前节点为叶子节点
+		for (int i = 0; i < top; i++)
+			HC[root][i] = stack[i];
+		HC[root][top] = '\0';
+		top--;
+		return;
+	}
+	HuffmanCoding(HT, HT[root].lchild, stack, 0, top);
+	HuffmanCoding(HT, HT[root].rchild, stack, 1, top);
+}
+
+void Cal_unsigned(char zero_one[], unsigned char& a)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		a = a * 2;
+		if (zero_one[i] == '1') a++;
+	}
+}
+
+void EnCoding(char** argv)
+{
+	ifstream inFile(argv[1], ios::in | ios::binary);
+	ofstream outFile("CodeFile1.txt", ios::out | ios::binary);//单纯存储Huffman编码后的文件
+	char zero_one[10];//临时存储01编码序列
+	int cnt_01 = 0;
+	unsigned char a = 0;
+	unsigned char tmp;
+	while (inFile.read((char*)&tmp, sizeof(tmp))) {
+		for (int i = 1; i <= num_char; i++) {
+			if (tmp == Char[i]) {
+				for (int j = 0; HC[i][j] != '\0'; j++) {
+					zero_one[cnt_01] = HC[i][j];
+					cnt_01++;
+					if (cnt_01 == 8) {//达到一个字节，可以存入
+						Cal_unsigned(zero_one, a);
+						outFile.write((char*)&a, sizeof(a));
+						a = 0;
+						cnt_01 = 0;
+						num_8++;
+					}
+				}
+			}
+		}
+	}
+	if (cnt_01) {//不够一个字节，末尾补零
+		num_0 = 8 - cnt_01;
+		for (int i = 0; i < cnt_01; i++)
+		{
+			a = a * 2;
+			if (zero_one[i] == '1') a++;
+		}
+		for (int i = 0; i < num_0; i++)
+			a = a * 2;
+		outFile.write((char*)&a, sizeof(a));
+	}
+	inFile.close();
+	outFile.close();
+}
+
+bool HuffmanDeCoding(FILE* fp1, FILE* fp2, HuffTree HT, int root)
+{
+	char p;
+	if (!fp1 || !fp2) {
+		cout << "error";
+		return 0;
+	}
+	if (HT[root].lchild == 0) {
+		fprintf(fp2, "%c", Char[root]);
+		return 1;
+	}
+	else {
+		p = fgetc(fp1);
+		if (p == EOF) return 0;
+	}
+	if (p == '0') HuffmanDeCoding(fp1, fp2, HT, HT[root].lchild);
+	if (p == '1') HuffmanDeCoding(fp1, fp2, HT, HT[root].rchild);
+	return 1;
+}
+
+void DeCoding(HuffTree HT, char** argv)
+{
+	FILE* fp, * fp2;
+	fp = fopen("CodeFile2.txt", "rb");
+	fp2 = fopen("TempTxt.txt", "w");
+	unsigned char p = fgetc(fp);
+	while (num_8) {
+		int cnt = 128;
+		int a = (int)p;
+		while (cnt) {
+			if (a >= cnt) {
+				a = a - cnt;
+				fputc('1', fp2);
+			}
+			else {
+				fputc('0', fp2);
+			}
+			cnt = cnt / 2;
+		}
+		p = fgetc(fp);
+		num_8--;
+	}
+	if (num_0) {
+		int cnt = 128;
+		int tmp = 8;
+		int a = (int)p;
+		while (tmp > num_0) {
+			if (a >= cnt) {
+				a = a - cnt;
+				fputc('1', fp2);
+			}
+			else {
+				fputc('0', fp2);
+			}
+			cnt = cnt / 2;
+			tmp--;
+		}
+	}
+	fclose(fp);
+	fclose(fp2);
+	FILE* fp1;
+	fp1 = fopen("TempTxt.txt", "r");
+	fp2 = fopen(argv[2], "wb");
+	while (HuffmanDeCoding(fp1, fp2, HT, 2 * num_char - 1));
+	fclose(fp1);
+	fclose(fp2);
+}
+
+void Store_all(HuffTree HT)
+{
+	FILE* fp1;
+	fp1 = fopen("CodeFile.txt", "w");
+	fprintf(fp1, "%d %d ", num_8, num_0);
+	fprintf(fp1, "%d ", num_char);
+	for (int i = 1; i <= 2 * num_char - 1; i++)
+	{
+		fprintf(fp1, "%d %d %d %d ", HT[i].weight, HT[i].parent, HT[i].lchild, HT[i].rchild);
+	}
+	char tmpp = EOF;
+	fprintf(fp1, "%c", tmpp);//前后间断点
+	fclose(fp1);
+
+	fp1 = fopen("CodeFile.txt", "ab");
+	for (int i = 1; i <= num_char; i++) fprintf(fp1, "%c", Char[i]);
+
+	ifstream inFile("CodeFile1.txt", ios::in | ios::binary);
+	unsigned char tmp;
+	while (inFile.read((char*)&tmp, sizeof(tmp))) {
+		fprintf(fp1, "%c", tmp);
+	}
+	inFile.close();
+	fclose(fp1);
+}
+
+void Read_Text(HuffTree& HT)
+{
+	FILE* fp1, * fp2;
+	fp1 = fopen("Pre_Info1.txt", "wb");
+	ifstream inFile("CodeFile.txt", ios::in | ios::binary);
+	unsigned char tmp;
+	while (inFile.read((char*)&tmp, sizeof(tmp))) {
+		if ((int)tmp == 255) break;
+		fprintf(fp1, "%c", tmp);
+	}
+	fclose(fp1);
+
+	fp1 = fopen("Pre_Info1.txt", "r");
+	fscanf(fp1, "%d%d%d", &num_8, &num_0, &num_char);
+	HT = new HTNode[2 * num_char];
+	for (int i = 1; i <= 2 * num_char - 1; i++) {
+		fscanf(fp1, "%d%d%d%d", &HT[i].weight, &HT[i].parent, &HT[i].lchild, &HT[i].rchild);
+	}
+	fclose(fp1);
+
+	fp2 = fopen("Pre_Info2.txt", "wb");
+	for (int i = 1; i <= num_char; i++) {
+		inFile.read((char*)&tmp, sizeof(tmp));
+		fprintf(fp2, "%c", tmp);
+	}
+	fclose(fp2);
+
+	ofstream outFile("CodeFile2.txt", ios::out | ios::binary);//单纯存储Huffman编码后的文件
+	while (inFile.read((char*)&tmp, sizeof(tmp))) {
+		outFile.write((char*)&tmp, sizeof(tmp));
+	}
+	inFile.close();
+	outFile.close();
+}
+
+void Load_Info(HuffTree& HT)
+{
+	ifstream inFile("Pre_Info2.txt", ios::in | ios::binary);
+	unsigned char tmp;
+	int cnt_char = 1;
+	while (inFile.read((char*)&tmp, sizeof(tmp))) {
+		Char[cnt_char] = (unsigned char)tmp;
+		cnt_char++;
+	}
+	inFile.close();
+}
+
+void show_menu()
+{
+	cout << "========HuffmanCoding========" << endl;
+	cout << "=====E:对文件编码============" << endl;
+	cout << "=====D:对文件解码============" << endl;
+	cout << "=====P:打印Huffman树=========" << endl;
+	cout << "=====Q:退出编码系统==========" << endl;
+	cout << "请输入指令：";
+}
+
+void Tree_Print(HuffTree HT, int root, int cnt)
+{
+	for (int i = 1; i <= cnt; i++)
+		cout << "     ";
+	if (root <= num_char) cout << (int)Char[root];
+	else cout << root;
+	cout << endl;
+	if (HT[root].lchild == 0) return;
+	Tree_Print(HT, HT[root].lchild, cnt + 1);
+	Tree_Print(HT, HT[root].rchild, cnt + 1);
+	return;
+}
